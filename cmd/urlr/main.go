ab324fc458650bce8624b7de5255f49054f672b5
Mason <masoncfrancis@gmail.com>
2024-04-21 00:08:04 -0600

    Added basic db stuff and flag parsing

--- cmd/urlr/main.go
+++ cmd/urlr/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"net/http"
 )
 
-func main() {
+type URLrecord struct {
+	gorm.Model
+	ShortURL string
+	LongURL  string
+}
 
-	// Check for db
+func main() {
+	// Check for sqlite db
+	db, err := gorm.Open(sqlite.Open("urlr.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&URLrecord{})
+
+	// Parse flags
+	addFlag := flag.String("add", "", "Add a new URL")
+	listFlag := flag.Bool("list", false, "List all URLs")
+	removeFlag := flag.String("remove", "", "Remove a URL")
+	flag.Parse()
 
 }
