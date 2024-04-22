26c0e09708adb782c8cce9605506194ba1b76ca5
Mason <masoncfrancis@gmail.com>
2024-04-21 18:22:30 -0600

    Added build scripts

---
+++ build/buildAllArchitectures.ps1
@@ -0,0 +1,91 @@
+param(
+    [string]$ProjectDirectory,
+    [string]$Version
+)
+
+# Check if project directory and version number are provided
+if (-not $ProjectDirectory -or -not $Version) {
+    Write-Host "Usage: .\build.ps1 -ProjectDirectory <project_directory> -Version <version>"
+    exit 1
+}
+
+# Check if project directory exists
+if (-not (Test-Path $ProjectDirectory -PathType Container)) {
+    Write-Host "Project directory not found: $ProjectDirectory"
+    exit 1
+}
+
+# Build for darwin/amd64
+$env:GOOS="darwin"
+$env:GOARCH="amd64"
+go build -o "out\urled_darwin_amd64_v$Version" $ProjectDirectory
+
+# Build for darwin/arm64
+$env:GOOS="darwin"
+$env:GOARCH="arm64"
+go build -o "out\urled_darwin_arm64_v$Version" $ProjectDirectory
+
+# Build for freebsd/386
+$env:GOOS="freebsd"
+$env:GOARCH="386"
+go build -o "out\urled_freebsd_386_v$Version" $ProjectDirectory
+
+# Build for freebsd/amd64
+$env:GOOS="freebsd"
+$env:GOARCH="amd64"
+go build -o "out\urled_freebsd_amd64_v$Version" $ProjectDirectory
+
+# Build for freebsd/arm
+$env:GOOS="freebsd"
+$env:GOARCH="arm"
+go build -o "out\urled_freebsd_arm_v$Version" $ProjectDirectory
+
+# Build for linux/386
+$env:GOOS="linux"
+$env:GOARCH="386"
+go build -o "out\urled_linux_386_v$Version" $ProjectDirectory
+
+# Build for linux/amd64
+$env:GOOS="linux"
+$env:GOARCH="amd64"
+go build -o "out\urled_linux_amd64_v$Version" $ProjectDirectory
+
+# Build for linux/armv5
+$env:GOOS="linux"
+$env:GOARCH="arm"
+$env:GOARM="5"
+go build -o "out\urled_linux_armv5_v$Version" $ProjectDirectory
+
+# Build for linux/armv6
+$env:GOOS="linux"
+$env:GOARCH="arm"
+$env:GOARM="6"
+go build -o "out\urled_linux_armv6_v$Version" $ProjectDirectory
+
+# Build for linux/armv7
+$env:GOOS="linux"
+$env:GOARCH="arm"
+$env:GOARM="7"
+go build -o "out\urled_linux_armv7_v$Version" $ProjectDirectory
+
+# Build for linux/arm64
+$env:GOOS="linux"
+$env:GOARCH="arm64"
+go build -o "out\urled_linux_arm64_v$Version" $ProjectDirectory
+
+# Build for windows/386
+$env:GOOS="windows"
+$env:GOARCH="386"
+go build -o "out\urled_windows_386_v$Version.exe" $ProjectDirectory
+
+# Build for windows/amd64
+$env:GOOS="windows"
+$env:GOARCH="amd64"
+go build -o "out\urled_windows_amd64_v$Version.exe" $ProjectDirectory
+
+# Build for windows/arm
+$env:GOOS="windows"
+$env:GOARCH="arm"
+go build -o "out\urled_windows_arm_v$Version.exe" $ProjectDirectory
+
+Write-Host "Builds completed."
--- build/buildAllArchitectures.sh
+++ build/buildAllArchitectures.sh
@@ -8,7 +8,7 @@ fi
 
 PROJECT_DIR="$1"
 VERSION="$2"
-PROJECT_NAME=$(basename "$PROJECT_DIR")
+PROJECT_NAME="urled"
 
 # Ensure the project directory exists
 if [ ! -d "$PROJECT_DIR" ]; then
@@ -17,51 +17,51 @@ if [ ! -d "$PROJECT_DIR" ]; then
 fi
 
 # Build for darwin/386
-env GOOS=darwin GOARCH=386 go build -o "./${PROJECT_NAME}_darwin_386_v${VERSION}" "$PROJECT_DIR"
+env GOOS=darwin GOARCH=386 go build -o "./out/${PROJECT_NAME}_darwin_386_v${VERSION}" "$PROJECT_DIR"
 
 # Build for darwin/amd64
-env GOOS=darwin GOARCH=amd64 go build -o "./${PROJECT_NAME}_darwin_amd64_v${VERSION}" "$PROJECT_DIR"
+env GOOS=darwin GOARCH=amd64 go build -o "./out/${PROJECT_NAME}_darwin_amd64_v${VERSION}" "$PROJECT_DIR"
 
 # Build for darwin/arm
-env GOOS=darwin GOARCH=arm go build -o "./${PROJECT_NAME}_darwin_arm_v${VERSION}" "$PROJECT_DIR"
+env GOOS=darwin GOARCH=arm go build -o "./out/${PROJECT_NAME}_darwin_arm_v${VERSION}" "$PROJECT_DIR"
 
 # Build for darwin/arm64
-env GOOS=darwin GOARCH=arm64 go build -o "./${PROJECT_NAME}_darwin_arm64_v${VERSION}" "$PROJECT_DIR"
+env GOOS=darwin GOARCH=arm64 go build -o "./out/${PROJECT_NAME}_darwin_arm64_v${VERSION}" "$PROJECT_DIR"
 
 # Build for freebsd/386
-env GOOS=freebsd GOARCH=386 go build -o "./${PROJECT_NAME}_freebsd_386_v${VERSION}" "$PROJECT_DIR"
+env GOOS=freebsd GOARCH=386 go build -o "./out/${PROJECT_NAME}_freebsd_386_v${VERSION}" "$PROJECT_DIR"
 
 # Build for freebsd/amd64
-env GOOS=freebsd GOARCH=amd64 go build -o "./${PROJECT_NAME}_freebsd_amd64_v${VERSION}" "$PROJECT_DIR"
+env GOOS=freebsd GOARCH=amd64 go build -o "./out/${PROJECT_NAME}_freebsd_amd64_v${VERSION}" "$PROJECT_DIR"
 
 # Build for freebsd/arm
-env GOOS=freebsd GOARCH=arm go build -o "./${PROJECT_NAME}_freebsd_arm_v${VERSION}" "$PROJECT_DIR"
+env GOOS=freebsd GOARCH=arm go build -o "./out/${PROJECT_NAME}_freebsd_arm_v${VERSION}" "$PROJECT_DIR"
 
 # Build for linux/386
-env GOOS=linux GOARCH=386 go build -o "./${PROJECT_NAME}_linux_386_v${VERSION}" "$PROJECT_DIR"
+env GOOS=linux GOARCH=386 go build -o "./out/${PROJECT_NAME}_linux_386_v${VERSION}" "$PROJECT_DIR"
 
 # Build for linux/amd64
-env GOOS=linux GOARCH=amd64 go build -o "./${PROJECT_NAME}_linux_amd64_v${VERSION}" "$PROJECT_DIR"
+env GOOS=linux GOARCH=amd64 go build -o "./out/${PROJECT_NAME}_linux_amd64_v${VERSION}" "$PROJECT_DIR"
 
 # Build for linux/armv5
-env GOOS=linux GOARCH=arm GOARM=5 go build -o "./${PROJECT_NAME}_linux_armv5_v${VERSION}" "$PROJECT_DIR"
+env GOOS=linux GOARCH=arm GOARM=5 go build -o "./out/${PROJECT_NAME}_linux_armv5_v${VERSION}" "$PROJECT_DIR"
 
 # Build for linux/armv6
-env GOOS=linux GOARCH=arm GOARM=6 go build -o "./${PROJECT_NAME}_linux_armv6_v${VERSION}" "$PROJECT_DIR"
+env GOOS=linux GOARCH=arm GOARM=6 go build -o "./out/${PROJECT_NAME}_linux_armv6_v${VERSION}" "$PROJECT_DIR"
 
 # Build for linux/armv7
-env GOOS=linux GOARCH=arm GOARM=7 go build -o "./${PROJECT_NAME}_linux_armv7_v${VERSION}" "$PROJECT_DIR"
+env GOOS=linux GOARCH=arm GOARM=7 go build -o "./out/${PROJECT_NAME}_linux_armv7_v${VERSION}" "$PROJECT_DIR"
 
 # Build for linux/arm64
-env GOOS=linux GOARCH=arm64 go build -o "./${PROJECT_NAME}_linux_arm64_v${VERSION}" "$PROJECT_DIR"
+env GOOS=linux GOARCH=arm64 go build -o "./out/${PROJECT_NAME}_linux_arm64_v${VERSION}" "$PROJECT_DIR"
 
 # Build for windows/386
-env GOOS=windows GOARCH=386 go build -o "./${PROJECT_NAME}_windows_386_v${VERSION}.exe" "$PROJECT_DIR"
+env GOOS=windows GOARCH=386 go build -o "./out/${PROJECT_NAME}_windows_386_v${VERSION}.exe" "$PROJECT_DIR"
 
 # Build for windows/amd64
-env GOOS=windows GOARCH=amd64 go build -o "./${PROJECT_NAME}_windows_amd64_v${VERSION}.exe" "$PROJECT_DIR"
+env GOOS=windows GOARCH=amd64 go build -o "./out/${PROJECT_NAME}_windows_amd64_v${VERSION}.exe" "$PROJECT_DIR"
 
 # Build for windows/arm
-env GOOS=windows GOARCH=arm go build -o "./${PROJECT_NAME}_windows_arm_v${VERSION}.exe" "$PROJECT_DIR"
+env GOOS=windows GOARCH=arm go build -o "./out/${PROJECT_NAME}_windows_arm_v${VERSION}.exe" "$PROJECT_DIR"
 
 echo "Builds completed."
